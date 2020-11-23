af2597c8da398525800f77c6fd31ffcd680421cd
Alexandra Stricklin <stricklin927@gmail.com>
2020-11-23 00:39:53 -0500

    edit intern js file

--- develop/lib/manager.js
+++ develop/lib/manager.js
@@ -1,6 +1,6 @@
 // TODO: Write code to define and export the Manager class. HINT: This class should inherit from Employee.
 
-const Employee = require("./Employee");
+const Employee = requre("./Employee");
 
 class Manager extends Employee {
     constructor(name, id, email, officeNumber){
